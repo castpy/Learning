288fa60cb4f872301127b9e93db52c202bbda8fd
Marcus Castilho <rootmarcus001@gmail.com>
2022-01-10 13:05:39 -0300

    Update Exercicio_48.py

--- CursoEmVideo/Modulo02/Exercicio_48.py
+++ CursoEmVideo/Modulo02/Exercicio_48.py
@@ -1,3 +1,8 @@
-for c in range (1, 501):
-    if (c % 2) == 1:
-        pass
+soma = 0
+cont = 0
+for c in range (1, 501, 2):
+    if (c % 3) == 0:
+        soma += c
+        cont += 1
+print('A soma de todos os valores é {}'.format(soma))
+print('Foram encontrados um total de {} números impares multiplos de 3'.format(cont))
