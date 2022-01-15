d63cf122048e3644a659de67f0898307633e0453
Marcus Castilho <rootmarcus001@gmail.com>
2022-01-15 17:56:25 -0300

    Update Exercicio_52.py

--- CursoEmVideo/Modulo02/Exercicio_52.py
+++ CursoEmVideo/Modulo02/Exercicio_52.py
@@ -8,3 +8,7 @@ for c in range(1, n1 + 1):
         print('\033[31m', end=' ')
     print('{}'.format(c), end='')
 print('\n\033[mO número {} foi divisível {} vezes.'.format(n1, total))
+if total == 2:
+    print('E por isso ele é PRIMO!')
+else:
+    print('E por isso ele NÃO é PRIMO!')
