2fbb0570f2b1833257da2c4167baa1e9ec082375
Marcus Castilho <rootmarcus001@gmail.com>
2022-01-20 13:40:02 -0300

    Create Exercicio_61.py

---
+++ Escolas/Curso Em Video/Python/Mundo 02/Exercicio_61.py
@@ -0,0 +1,10 @@
+print('=' * 20)
+print('10 termos de uma PA')
+print('=' * 20)
+
+n1 = int(input('Prieiro  termo: '))
+n2 = int(input('Digite a razão: '))
+dec = n1 + (10 -1) * n2
+
+while not False:
+    print()
