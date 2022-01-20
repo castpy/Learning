8817b14f9d73784581ce43d04b4cd843a15f0eff
Marcus Castilho <rootmarcus001@gmail.com>
2022-01-20 13:48:43 -0300

    Aulas já concluídas

--- Escolas/Curso Em Video/Python/Mundo 02/Exercicio_61.py
+++ Escolas/Curso Em Video/Python/Mundo 02/Exercicio_61.py
@@ -1,10 +1,15 @@
 print('=' * 20)
-print('10 termos de uma PA')
+print('10 termos de uma PA - Com While')
 print('=' * 20)
 
 n1 = int(input('Prieiro  termo: '))
 n2 = int(input('Digite a razão: '))
 dec = n1 + (10 -1) * n2
+termo = n1
+contador = 1
 
-while not False:
-    print()
+while contador <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += n2
+    contador += 1
+print('ACABOU')
