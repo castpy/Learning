37989e642c8e2c7e78a2d7e43a81024dd214b2ec
Marcus Castilho <rootmarcus001@gmail.com>
2022-01-30 15:42:16 -0300

    Aulas já conluídas

---
+++ Back-End/Curso de Python/Mundo 02/Exercicio_68.py
@@ -0,0 +1,29 @@
+from random import randint
+
+v = 0
+
+while True:
+    jogador = int(input('Digite um valor: '))
+    cpu = randint(0, 10)
+    total = jogador + cpu
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Impar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador jogou {cpu}. Total de {total}')
+
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você ganhou!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você ganhou!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    print('Vamos jogar novamente....')
+print(f'Você perdeu e sua pontuação é {v}')
