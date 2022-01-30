e4d16f8254c0f4bcb0ea219efde9d132f6480578
Marcus Castilho <rootmarcus001@gmail.com>
2022-01-30 15:46:08 -0300

    Aulas já concluídas

--- Back-End/Curso de Python/Mundo 02/Exercicio_68.py
+++ Back-End/Curso de Python/Mundo 02/Exercicio_68.py
@@ -9,7 +9,7 @@ while True:
     tipo = ' '
     while tipo not in 'PI':
         tipo = str(input('Par ou Impar? [P/I] ')).strip().upper()[0]
-    print(f'Você jogou {jogador} e o computador jogou {cpu}. Total de {total}')
+    print(f'Você jogou {jogador} e o computador jogou {cpu}. Total de {total}', end='')
     print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
 
     if tipo == 'P':
