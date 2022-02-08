febc03c9291fe60da6b3cd9672311baf33f5ed0c
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-08 15:55:58 -0300

    Update Aula_16.py

--- Back-End/Curso de Python/Mundo 03/Aula_16.py
+++ Back-End/Curso de Python/Mundo 03/Aula_16.py
@@ -7,3 +7,6 @@ lanche = 'café'
 
 # Temos três formas de fazer uma variável composta... Através de (TUPLAS, LISTAS e DICIONÁRIOS)
 # Vamos ver como criar uma TUPLA
+
+# OBS: As tuplas são imutaveis, ou seja, elas não tem como mudar o seu valor!
+# As tuplas são feitas com parenteses ()
