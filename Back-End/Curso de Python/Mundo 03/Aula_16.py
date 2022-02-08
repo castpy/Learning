25d6b34e0ba4b457c224c15694ff30f90a7faa41
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-08 17:10:46 -0300

    Tuplas

--- Back-End/Curso de Python/Mundo 03/Aula_16.py
+++ Back-End/Curso de Python/Mundo 03/Aula_16.py
@@ -3,10 +3,24 @@
 # Até então, tinhamos aprendido somente um tipo de atribuir valor a uma variável. Era com variável simples
 
 # Variáveis simples:
-lanche = 'café'
+lanche01 = 'café'
 
 # Temos três formas de fazer uma variável composta... Através de (TUPLAS, LISTAS e DICIONÁRIOS)
 # Vamos ver como criar uma TUPLA
 
 # OBS: As tuplas são imutaveis, ou seja, elas não tem como mudar o seu valor!
 # As tuplas são feitas com parenteses ()
+
+lanche = ('Nescal', 'Coxinha', 'Pão', 'Ovo')
+print(lanche)
+print(lanche[0])
+print(lanche[1:4])
+print(lanche[0:4:2])
+print(lanche[-1])
+
+print('='*10)
+for c in lanche:
+    print(c)
+print('='*10)
+for cont in range(len(lanche)):
+    print(lanche[cont])
