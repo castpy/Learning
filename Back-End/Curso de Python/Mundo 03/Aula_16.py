4071ea7252f2acd083b7c84a020d525582961ca8
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-08 10:21:43 -0300

    Create Aula_16.py

---
+++ Back-End/Curso de Python/Mundo 03/Aula_16.py
@@ -0,0 +1,9 @@
+#               Variaveis compostas (tuplas)
+# As tuplas servem básicamente para atribuir mais de um valor a uma única variável.
+# Até então, tinhamos aprendido somente um tipo de atribuir valor a uma variável. Era com variável simples
+
+# Variáveis simples:
+lanche = 'café'
+
+# Temos três formas de fazer uma variável composta... Através de (TUPLAS, LISTAS e DICIONÁRIOS)
+# Vamos ver como criar uma TUPLA
