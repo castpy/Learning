7e4c75d79e3feca56bf2543c6b71c48a64c179a0
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-14 17:53:59 -0300

    Update Exercicio_78.py

--- Escolas/Curso em Video/Back-End/Curso de Python/Mundos/Mundo 03/Exercicio_78.py
+++ Escolas/Curso em Video/Back-End/Curso de Python/Mundos/Mundo 03/Exercicio_78.py
@@ -1,4 +1,8 @@
 valores = list()
 for i in range(0,5):
-    valores.append(int(input('Digite um número: ')))
-print(f'O maior número foi {max(valores)} e o menor foi {min(valores)}')
+    valores.append(int(input(f'Digite um valor para a posição {i}: ')))
+
+print('=' * 30)
+print(f'Você digitou os valores {valores}')
+print(f'O maior valor é {max(valores)} e está na posição {...}')
+print(f'O menor valor é {min(valores)} e está na posição {...}')
