94c1dc2968eb2a7c3313fb815e99e9357667605d
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-08 13:40:44 -0300

    Calculadora de Média

---
+++ Escolas/Alura/Imersao Dev/Dia 01/main.js
@@ -0,0 +1,13 @@
+var nome = "Usuário"
+console.log(nome)
+alert("Bem vindo, " + nome + "!")
+
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 10
+var notaTerceiroBimestre = 10
+var notaQuartoBimestre = 8
+
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+alert('Sua nota foi ' + notaFinal)
