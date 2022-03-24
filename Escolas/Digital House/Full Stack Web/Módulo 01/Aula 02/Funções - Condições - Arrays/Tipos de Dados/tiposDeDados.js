3bb73b2bcd0d5e1c9dc350379abffa5cee92fb33
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-24 14:41:08 -0300

    Trabalhando com os tipos de dados da linguagem JS

---
+++ Escolas/Digital House/Full Stack Web/Módulo 01/Aula 02/Funções - Condições - Arrays/Tipos de Dados/tiposDeDados.js
@@ -0,0 +1,53 @@
+
+//String aspas Duplas
+let nome = "Marcus";
+console.log(nome)
+
+//String aspas Simples
+let sobreNome = 'Vinicius';
+console.log(sobreNome)
+
+// OBS: Não há nenhum tipo de alteração quando usamos aspas duplas ou simples!
+
+//Numeros inteiros
+let idade = 17;
+console.log(idade)
+
+//Numeros decimais
+let altura = 1.75; // OBS: Como o JS foi desenvolvido em Inglês, não usamos virgula "," nos numeros decimais e sim ponto "."
+console.log(altura)
+
+//Valor Boleano
+let diaChuvoso = false;
+console.log('O dia está chuvoso? ' + diaChuvoso);
+
+//Valor Not a Number
+let naoNumerico = NaN
+console.log(naoNumerico)
+
+//Valores Null
+let valorNull = null;
+console.log(valorNull)
+
+//Valor indefinido
+let valorIndefinido = undefined;
+//valorIndefinido = 10
+//Para mudar o valor de "valorIndefinido", basta remover o comentario a cima.
+console.log(valorIndefinido)
+
+//Objetos literais
+let objetoLiteral = {
+    marca: "Honda",
+    kmRodado: 5.000,
+    corDaMoto: "Black",
+    produtoNovo: false
+}
+console.log(objetoLiteral)
+
+//Listas
+let listaDeCompras = [
+    "Macarrão",
+    "Carne",
+    "Refrigerante"
+]
+console.log(listaDeCompras[0])
