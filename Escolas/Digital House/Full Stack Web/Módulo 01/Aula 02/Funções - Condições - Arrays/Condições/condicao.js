61c0ce0e4f216178e859d6991755a8c4241d2abe
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-25 17:46:27 -0300

    Trabalhando com condições IF

---
+++ Escolas/Digital House/Full Stack Web/Módulo 01/Aula 02/Funções - Condições - Arrays/Condições/condicao.js
@@ -0,0 +1,9 @@
+let dia = "Quarta";
+
+if (dia == "Domingo") {
+    console.log("Irei para a praia!")
+}else if (dia == "Quarta") {
+    console.log("Irei para o cinema")
+}else {
+    console.log("Ficarei em casa!")
+}
