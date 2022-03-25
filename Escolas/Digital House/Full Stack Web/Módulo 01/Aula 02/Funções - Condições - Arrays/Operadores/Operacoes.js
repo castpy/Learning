1b04256b9492a96519f5592f017a53f83adf23dd
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-25 12:57:20 -0300

    Tipos de operadores

---
+++ Escolas/Digital House/Full Stack Web/Módulo 01/Aula 02/Funções - Condições - Arrays/Operadores/Operacoes.js
@@ -0,0 +1,19 @@
+//adição
+console.log(2+2);
+
+//subtração
+console.log(5-5);
+
+//multiplicação
+console.log(10*2);
+
+//divisão
+console.log(10/2);
+
+//incremento
+let numero = 10;
+console.log(++ numero)
+
+//decremento
+let numero02 = 10
+console.log(-- numero02)
