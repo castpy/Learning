5d3975256baeef2dac2690a67b821617d7d44dd6
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-25 18:04:18 -0300

    Trabalhando com Array

---
+++ Escolas/Digital House/Full Stack Web/Módulo 01/Aula 02/Funções - Condições - Arrays/Array/array.js
@@ -0,0 +1,12 @@
+let comidasParaense = [
+    "Açaí", //Indice 0
+    "Farinha", //Indice 1
+    "Maniçoba", //Indice 2
+    "Peixe Frito", //Indice 3
+    "Pato no tucupi" //Indice 4
+];
+
+console.log('Minha comida favorita é ' + comidasParaense[0] + ' com ' + comidasParaense[3])
+
+// Para descobrir o tamanho do array, basta usar o seguinte comando:
+//console.log(comidasParaense.length)
