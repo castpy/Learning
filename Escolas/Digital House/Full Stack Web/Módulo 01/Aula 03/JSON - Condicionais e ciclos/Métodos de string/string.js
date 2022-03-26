7d544581cfbf7bfaddbe5af7d15691108e9ef872
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-26 14:49:33 -0300

    Trabalhando com métodos de string

---
+++ Escolas/Digital House/Full Stack Web/Módulo 01/Aula 03/JSON - Condicionais e ciclos/Métodos de string/string.js
@@ -0,0 +1,6 @@
+console.log("Olá, mundo!".length) //retornar o tamanho da string
+console.log("Olá, mundo!".slice(0,3)) //retorna toda string contida dos indices 0 à 3, sabendo que irá contar até o 2
+console.log("Olá, mundo!".indexOf("mundo")) //procura pela string passada como parametro e retorna seu indice
+console.log(" Olá, mundo! ".trim()) //remove todo espaço antes e depois da string
+console.log("Olá, mundo!".split(" ")) //transforma a string em um array, delimitando cada objeto do array com a string passada como parametro no método .split()
+console.log("Olá, mundo!".replace("mundo", "Marcus")) //remove a string igual a do primeiro valor passado como parametro e muda para uma string com o valor do segundo parametro
