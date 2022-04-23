f6c36cd4bb199eb7a8944b577d5e0709fdd57e4d
Marcus Castilho <rootmarcus001@gmail.com>
2022-04-23 15:40:50 -0300

    Aula 03

---
+++ Escolas/Recode/Desenvolvimento WEB - Básico/JS/Aula_03/01 - Loop/001.js
@@ -0,0 +1,16 @@
+//let contador = 0;
+
+//while(contador <= 1000){
+//    console.log(contador);
+//    contador++;
+//}
+
+let tabuada = 7;
+let contador = 1;
+
+while(contador <= 10){
+    console.log(
+        tabuada, ' x ', contador, ' = ',
+        tabuada * contador++ 
+    )
+}
---
+++ Escolas/Recode/Desenvolvimento WEB - Básico/JS/Aula_03/01 - Loop/002.js
@@ -0,0 +1,11 @@
+//for(let contador = 0; contador <= 10; contador++){
+//    console.log(contador)
+//}
+
+let tabuada = 9;
+ for(let contador = 1; contador <= 10; contador++){
+     console.log(
+         tabuada, ' x ', contador, ' = ',
+         tabuada * contador
+     )
+ }
