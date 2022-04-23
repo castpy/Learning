ed6c99b3fcad1670be0070f8504a727cf029c469
Marcus Castilho <rootmarcus001@gmail.com>
2022-04-23 14:40:54 -0300

    Conclusão da aula 02

---
+++ Escolas/Recode/Desenvolvimento WEB - Básico/JS/Aula_02/03 - Condições/001.js
@@ -0,0 +1,11 @@
+let nome = 'Marcus';
+let email = 'marcus@castilho.com';
+let senha = 9191;
+
+if(nome == 'Marcus' && senha == 9191){
+    console.log('Usuário logado com sucesso!')
+}else if(email == 'marcus@castilho.com' && senha == 9191){
+    console.log('Usuário logado com sucesso!')
+}else{
+    console.log('Usuario/email ou senha incorreto!')
+}
---
+++ Escolas/Recode/Desenvolvimento WEB - Básico/JS/Aula_02/04 - Switch/002.js
@@ -0,0 +1,27 @@
+let mes = 4;
+
+switch(mes){
+    case 1:
+    case 2:
+    case 3:
+        console.log('1º trimestre');
+        break;
+    case 4:
+    case 5:
+    case 6:
+        console.log('2º trimestre');
+        break;
+    case 7:
+    case 8:
+    case 9:
+        console.log('3º trimestre');
+        break;
+    case 10:
+    case 11:
+    case 12:
+        console.log('4º trimestre');
+        break;
+    
+    default:
+        console.log('Mês inválido!')
+}
