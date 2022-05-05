588815f6681c8861d4e417f6e54bde5576a34e51
Marcus Castilho <rootmarcus001@gmail.com>
2022-05-05 19:28:19 -0300

    Variaveis e tratamento de dados em JS

---
+++ Escolas/Curso em Video/Front-End/Curso de HTML e CSS/Curso de JavaScript/Módulos/Módulo - B/001 - Varivaeis e Tipos Primitivos/001.js
@@ -0,0 +1,9 @@
+var n1 = 10     //typeof = number
+var n2 = null   //typeof = object
+
+var nome = 'Marcus'     //typeof = string
+var bool = true         //typeof = boolean
+var inde = undefined    //typeof = undefined
+
+let objeto = []         //typeof = object
+let Array = {}          //typeof = object
