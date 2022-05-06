c68de65bf3ae7375575ac1392281324786fd48c7
Marcus Castilho <rootmarcus001@gmail.com>
2022-05-06 14:50:02 -0300

    Update 003.js

--- Escolas/Curso em Video/Front-End/Curso de HTML e CSS/Curso de JavaScript/Módulos/Módulo - B/001 - Varivaeis e Tipos Primitivos/003.js
+++ Escolas/Curso em Video/Front-End/Curso de HTML e CSS/Curso de JavaScript/Módulos/Módulo - B/001 - Varivaeis e Tipos Primitivos/003.js
@@ -9,3 +9,17 @@ var n = 3
 n = n * 3
 // n *= 3
 console.log(n)
+
+//Operadores relacionais
+var numero1 = 4
+var numero2 = 4
+console.log(numero1 > numero2) //Resultado: False
+console.log(numero1 < numero2) //Resultado: False
+console.log(numero1 >= numero2)//Resultado: True
+console.log(numero1 <= numero2)//Resultado: True
+console.log(numero1 == numero2)//Resultado: True
+console.log(numero1 != numero2)//Resultado: False
+
+//Operadores de Identidade
+console.log(`${numero1} == '4' -> A penas o valor é verificado. TRUE`)
+console.log(`${numero1} === '4' -> O valor e o tipo é verificado. FALSE`)
