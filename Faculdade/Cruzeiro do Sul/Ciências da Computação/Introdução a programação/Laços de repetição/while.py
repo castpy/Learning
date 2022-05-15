64e9ec5ac3a7b5f172a3fa81fe136a3a4d2625c9
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2022-05-15 13:28:45 -0300

    Faculdade removida

--- Faculdade/Cruzeiro do Sul/Ciências da Computação/Introdução a programação/Laços de repetição/while.py
+++
@@ -1,9 +0,0 @@
-nome = input('Qual seu nome? ')
-
-while True:
-    a = input(f'Olá {nome.upper()}! Para finalizar o script digite 0: ')
-    if a == '0':
-        print('Saindo...')
-        break
-    else: 
-        continue
