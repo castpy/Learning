4ffd1ce7c5425e4ec88577e26d13ba55a5dba3d6
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2022-06-21 21:02:36 -0300

    Dia 01/150

---
+++ Escolas/Alura/Front-end/JS/03.js
@@ -0,0 +1,20 @@
+//String
+
+const texto01 = "Olá, Mundo!";
+const texto02 = 'Olá, Mundo!';
+
+console.log(texto01)
+console.log(texto02)
+
+
+//Concatenação (+)
+
+const nome01 = 'Marcus';
+const nome02 = 'Castilho';
+console.log('Meu nome é ' + nome01 + ' ' + nome02)
+
+
+//Template String
+
+const N1 = 50;
+console.log(`No bar tinha ${N1} pessoas.`);
---
+++ Escolas/Alura/Front-end/JS/04.js
@@ -0,0 +1,5 @@
+//Boleano
+
+const N1 = 5;
+const N2 = 10;
+console.log(N1 === N2);
