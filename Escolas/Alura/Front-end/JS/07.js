34437bb70a539a8fbb74c42e8ea7a785736e8b36
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2022-06-23 20:48:39 -0300

    Dia 03/150

---
+++ Escolas/Alura/Front-end/JS/07.js
@@ -0,0 +1,11 @@
+//Conversão implícita
+
+const numero = 456;
+const numeroString = '456';
+console.log(numero == numeroString)
+
+
+//Conversão Explícita
+
+const numeroString2 = '02';
+console.log(Number(numeroString2 ))
